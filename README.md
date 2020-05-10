5e8eacdc943e3050b009210c791fec67a1dff753
Kayla Kuhlman <kakoreo4@gmail.com>
2020-05-09 19:48:14 -0500

    removed placeholders, uploaded to firebase

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dice-roller
-Dice Roller for Roleplaying and Board Games
+# This is How I Roll
+## Dice Roller
+
+To view the hosted project, please go to [https://this-is-how-i-roll.firebaseapp.com/](https://this-is-how-i-roll.firebaseapp.com/)
+
+### Description
+
+This single-page react application is a dice roller to help with role-playing or board games were multiple dice need to be rolled.
--- src/App.js
+++ src/App.js
@@ -7,22 +7,31 @@ function App() {
   let [diceType, setDiceType] = useState([])
   let [numberRolls, setNumberRolls] = useState([])
   let [modifier, setModifier] = useState([])
+  let [calculation, setCalculation] = useState([])
 
-  function d6roll(e) {
-    e.preventDefault();
+  function roll(e) {
     let sum = 0
     let currentRoll
     let allRolls = []
     let overallRoll
-    for (var i=0; i < numberRolls; i++) {
+    let calc = ""
+
+    e.preventDefault();
+    for (var i = 0; i < numberRolls; i++) {
       currentRoll = Math.floor((Math.random() * diceType) + 1)
       allRolls.push(currentRoll)
       sum = sum + currentRoll
       overallRoll = parseFloat(sum) + parseFloat(modifier)
       setDiceRoll(overallRoll)
     }
-  console.log(allRolls)
-  console.log(modifier)
+    console.log(allRolls)
+    console.log(modifier)
+    allRolls.forEach((element) => {
+      calc += `${element} + `
+    })
+     calc += `${modifier} = `
+    console.log(calc)
+    setCalculation(calc)
   }
 
 
@@ -34,7 +43,7 @@ function App() {
 
         <div className="form-group">
           <div className="row">
-            <div className="col-sm-1"></div>
+            <div className="col-sm-2"></div>
             <div className="col-sm-2">
               <h6>Dice Type</h6>
               <div className="radio">
@@ -57,23 +66,20 @@ function App() {
               </div>
             </div>
             <div className="col-sm-2">
-              <h6>Number of Rolls</h6>
-              <input type="number" className="form-control" id="numberRolls" onChange={e => setNumberRolls(e.target.value)} placeholder="1"></input>
+              <h6># of Rolls</h6>
+              <input type="number" className="form-control" id="numberRolls" defaultValue = "1" onChange={e => setNumberRolls(e.target.value)}></input>
             </div>
             <div className="col-sm-2">
               <h6>Modifier</h6>
-              <input type="number" className="form-control" id="Modifier" onChange={e => setModifier(e.target.value)}  placeholder="0"></input>
+              <input type="number" className="form-control" id="Modifier" defaultValue = "0" onChange={e => setModifier(e.target.value)}></input>
             </div>
-            <div className="col-sm-2">
-              <button type="submit" onClick={d6roll} className="btn btn-primary">Roll</button>
+            <div className="col-sm-2"><br></br>
+              <button type="submit" onClick={roll} className="btn btn-primary">Roll</button>
             </div>
-            <div className="col-sm-1"></div></div></div>
+            <div className="col-sm-2"></div></div></div>
 
       </form>
-
-{/*   <button type="button" onClick={d6roll} className="btn">Roll d6</button> */}
-
-      <div className="card"><h3>{ diceRoll }</h3></div>
+      <div className="card">{calculation}<h3>{diceRoll}</h3></div>
 
 
     </div>
